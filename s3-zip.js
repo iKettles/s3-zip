ba5c600cbeff0735c6509497d6290ecfa3ae917e
iKettles <iain@framer.com>
2019-09-15 18:10:48 +0200

    Add support for duplicate filenames in list

--- s3-zip.js
+++ s3-zip.js
@@ -35,7 +35,20 @@ s3Zip.archive = function (opts, folder, filesS3, filesZip) {
   return archive
 }
 
+function getAllIndices(arr, val) {
+  let indices = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      indices.push(i);
+    }
+  }
+
+  return indices;
+}
+
 s3Zip.archiveStream = function (stream, filesS3, filesZip) {
+  const processedIndices = [];
   const self = this
   const folder = this.folder || ''
   if (this.registerFormat) {
@@ -51,16 +64,33 @@ s3Zip.archiveStream = function (stream, filesS3, filesZip) {
         self.debug && console.log('don\'t append to zip', file.path)
         return
       }
+
       let fname
+
       if (filesZip) {
+        const filePathExcludingFolder = (!!folder && file.path.startsWith(folder)) ? file.path.substr(folder.length + 1) : file.path;
+        const fileIndices = getAllIndices(filesS3, filePathExcludingFolder);
+
+        let i = filesS3.indexOf(filePathExcludingFolder)
+
+        // Find duplicates of this filename
+        if (fileIndices.length > 1) {
+          const indexToProcess = fileIndices.filter(index => !processedIndices.includes(index))[0]
+          i = indexToProcess;
+          self.debug && console.log('Found duplicate file with path ' + file.path + ', assuming an index of ' + i)
+        }
+
         // Place files_s3[i] into the archive as files_zip[i]
-        const i = filesS3.indexOf(file.path.startsWith(folder) ? file.path.substr(folder.length) : file.path)
         fname = (i >= 0 && i < filesZip.length) ? filesZip[i] : file.path
+
+        processedIndices.push(i);
       } else {
         // Just use the S3 file name
         fname = file.path
       }
-      const entryData = typeof fname === 'object' ? fname : { name: fname }
+      const entryData = typeof fname === 'object' ? fname : {
+        name: fname
+      }
       self.debug && console.log('append to zip', fname)
       if (file.data.length === 0) {
         archive.append('', entryData)
